e42ea0d15c9688283f317a8f53b07e5efb29dfe1
Nicholas Kingzett <NkKnZt@gmail.com>
2022-12-11 00:56:14 -0500

    added a list subclass for hands with methods for total added draw functions

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pyxel
 from collections import namedtuple
+from random import shuffle
 
 SCREEN_WIDTH = 320
 SCREEN_HEIGHT = 180
@@ -28,8 +29,49 @@ ACTIONS_TEXT_LOC = Vec2(SCREEN_WIDTH - (9 * pyxel.FONT_WIDTH) - 4)
 
 STACK_DEFAULT = 500
 MIN_BET = 10
+SHOE_SIZE = 8
 
 DEAL_SPEED = CARD_WIDTH // 3    # in pixels per frame
+HAND_OFFSET = CARD_WIDTH // 3   # horizontal offset per card when drawing hand
+SHOE_OFFSET = CARD_WIDTH // 4   # horizontal offset per face down card when drawing the shoe
+
+class Hand(list):
+
+    def total(self, ignore_first=False):
+        total_ = 0
+        ace = False
+        if ignore_first:
+            if len(self) > 1:
+                for card_ in self[1:]:
+                    if not ace and card_.value == 1:
+                        ace = True
+                    total_ += min(card_.value, 10)
+        else:
+            for card_ in self:
+                if not ace and card_.value == 1:
+                    ace = True
+                total_ += min(card_.value, 10)
+        if total_ <= 11 and ace:
+            total_ += 10
+        return total_
+
+    def total_str(self, ignore_first=False):
+        total = 0
+        ace = False
+        if ignore_first:
+            if len(self) > 1:
+                for card_ in self[1:]:
+                    if not ace and card_.value == 1:
+                        ace = True
+                    total += min(card_.value, 10)
+        else:
+            for card_ in self:
+                if not ace and card_.value == 1:
+                    ace = True
+                total += min(card_.value, 10)
+        if total <= 11 and ace:
+            return f'/{total + 10}'
+        return str(total)
 
 
 def create_decks(num):
@@ -38,6 +80,7 @@ def create_decks(num):
         for s in range(4):
             for v in range(1, 14):
                 deck.append(Card(s, v))
+    shuffle(deck)
     return deck
 
 
@@ -50,6 +93,23 @@ def draw_card(card_, x, y, face_down=False, card_back=0):
         pyxel.blt(x, y, HIGH_BANK, (card_.value - 7) * CARD_WIDTH, card_.suit * CARD_HEIGHT, CARD_WIDTH, CARD_HEIGHT)
 
 
+def draw_hand(hand_, x, y, ignore_first=False):
+    # draw cards
+    for i, card_ in enumerate(hand_):
+        if ignore_first and i == 0:
+            draw_card(card_, x + i * HAND_OFFSET, y, face_down=True)
+        else:
+            draw_card(card_, x + i * HAND_OFFSET, y)
+    # draw total text
+    pyxel.text(x + CARD_WIDTH // 2, y - pyxel.FONT_HEIGHT - 3, hand_.total_str(ignore_first=ignore_first), pyxel.COLOR_YELLOW)
+
+
+def draw_shoe(shoe):
+    if shoe:
+        for i in range(min(len(shoe) // 50, 1)):
+            draw_card(Card(0, 1), SHOE_LOC.x + i * SHOE_OFFSET, SHOE_LOC.y, face_down=True)
+
+
 class App:
     def __init__(self):
         # TODO -- load settings and stack size from file?
@@ -59,6 +119,10 @@ class App:
         self.state = 'intro'    # states are: intro, bet, deal, player turn, dealer turn, payout
         self.bg_color = pyxel.COLOR_GREEN
         self.stack = STACK_DEFAULT
+        self.player_hand = Hand()
+        self.split_hand = Hand()
+        self.dealer_hand = Hand()
+        self.shoe = create_decks(SHOE_SIZE)
         pyxel.run(self.update, self.draw)
 
     def update(self):
@@ -83,15 +147,25 @@ class App:
         if self.state == 'intro':
             pass
         elif self.state == 'bet':
-            pass
+            draw_shoe(self.shoe)
+            # TODO -- draw bet text
         elif self.state == 'deal':
-            pass
+            draw_shoe(self.shoe)
+            draw_hand(self.player_hand, PLAYER_LOC.x, PLAYER_LOC.y)
+            draw_hand(self.dealer_hand, DEALER_LOC.x, DEALER_LOC.y, ignore_first=True)
         elif self.state == 'player':
-            pass
+            draw_shoe(self.shoe)
+            draw_hand(self.player_hand, PLAYER_LOC.x, PLAYER_LOC.y)
+            draw_hand(self.dealer_hand, DEALER_LOC.x, DEALER_LOC.y, ignore_first=True)
+            # TODO -- actions text
         elif self.state == 'dealer':
-            pass
+            draw_shoe(self.shoe)
+            draw_hand(self.player_hand, PLAYER_LOC.x, PLAYER_LOC.y)
+            draw_hand(self.dealer_hand, DEALER_LOC.x, DEALER_LOC.y)
         elif self.state == 'payout':
-            pass
+            draw_shoe(self.shoe)
+            draw_hand(self.player_hand, PLAYER_LOC.x, PLAYER_LOC.y)
+            draw_hand(self.dealer_hand, DEALER_LOC.x, DEALER_LOC.y)
 
     def deal_card(self):
         # TODO -- find distance between shoe location and destination
